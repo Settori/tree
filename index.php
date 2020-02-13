58fd4e0215e9693726093f7b147d9e914d1faa79
Settori <settoriq@gmail.com>
2020-02-13 11:58:30 +0100

    commit #2

--- index.php
+++ index.php
@@ -1,42 +1,45 @@
 <?php
 
-$tree = json_decode(file_get_contents('tree.json'), true);
-$list = json_decode(file_get_contents('list.json'), true);
+    class treeJson {
+        public $tree;
+        public $list;
+        public $names;
+        public $buf = array();
 
-function get_names($data) {
-    $return = array();
-    foreach ($data as $r) {
-        $return[$r['category_id']] = $r['translations']['pl_PL']['name'];
+        //Contructor
+        function __construct() {
+            $this->tree = json_decode(file_get_contents('tree.json'), true);
+            $this->list = json_decode(file_get_contents('list.json'), true);
+            $this->names = $this->get_names();
+        }
+
+        //Create array with pairs 'customer_id' : 'name'
+        function get_names() {
+            $return = array();
+            foreach ($this->list as $r) {
+                $return[$r['category_id']] = $r['translations']['pl_PL']['name'];
+            }
+            return $return;
+        }
+
+        //Add names to array
+        function add_names(&$data) {
+            foreach($data as &$d) {
+                if (isset($this->names[$d['id']]))                  //Check if there is 'name' with this 'id' in $names
+                    $d = ['name' => $this->names[$d['id']]] + $d;   //Add 'name' with value to the begining of array
+                else $d = ['name' => null] + $d;                    //If there is no name for this 'id' put null instead
+                $this->add_names($d['children']);               //Do these same for children
+                
+            }
+            return $data;
+
+        }
     }
-    return $return;
-}
 
+    //Create class object
+    $t = new treeJson;
 
-function test($data, $i, $n) {
-    
-    foreach($data as $r) {
-        array_push($data, array('test' => 'dupa'));
-        //for ($x=0; $x<$i; $x++) echo "---";
-        //echo $r['id'];
-        //if (isset($n[$r['id']])) echo ": " . $n[$r['id']];
-        //echo "<br>";
-        
-        test($r['children'], $i+1, $n);
-    }
-    return $data;
-}
-
-
-function test2($data) {
-    print_r($data);
-}
-
-
-//print_r(test($tree, 0, get_names($list)));
-
-
-//print_r(get_names($list));
-//test($tree, 0, get_names($list));
-//print_r(test($tree, 0, get_names($list)));
-//test2($tree);
+    //Create variable with json data
+    $json = json_encode($t->add_names($t->tree));       //Function add_names with data from 'tree'
+    file_put_contents('names.json', $json);            //Save new json with names to a file
 ?>
